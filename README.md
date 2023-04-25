892db2cf724d5204ab3181492c5a3a9993fab8fe
youngcheolLee <zodiac0905@gmail.com>
2023-04-26 02:17:27 +0900

    Update google.py 크롤링 기능 완성

--- README.md
+++ README.md
@@ -1,28 +1,35 @@
 # 크롬 크롤링 자동화테스트
 
 가상 환경 생성 </br>
-`phtyon -m venv "파일명"` 
+`phtyon -m venv "파일명"`
 
 ## 기술스택
+
 - python
 - selenium
 
 ## 구현기능
 
 ### 브라우저 강제 종료 방지 코드 추가
+
 ![image](https://user-images.githubusercontent.com/107835019/234078577-ecddcdc7-b68d-4a8a-9aad-b5f5e917c4b4.png)
 
 ### 크롬 실행 > google.com 접속
+
 ![image](https://user-images.githubusercontent.com/107835019/234078867-e05cbc9a-2671-4824-912b-d31cc511d7fa.png)
 
 ### 검색창 name property 명 확인
+
 ![image](https://user-images.githubusercontent.com/107835019/234079334-15bf0f12-3965-46d2-9b68-00b6ca89a394.png)
 
 ### 검색창 선택 코드 추가
+
 ![image](https://user-images.githubusercontent.com/107835019/234079537-acc2d436-15c6-4406-9297-fb6105f1c758.png)
 
 ### 검색창에 입력할 문구 추가
+
 ![image](https://user-images.githubusercontent.com/107835019/234079643-cf84ea98-2c12-4831-a584-91c8f19150ce.png)
 
 ### Enter key 코드 추가
+
 ![image](https://user-images.githubusercontent.com/107835019/234079825-90f1fe2f-0821-4832-86c5-7321245dd98e.png)
--- google.py
+++ google.py
@@ -2,15 +2,29 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+import os
+import time
+import urllib.request
+
+img_folder = os.path.abspath("Crawling_images")
 
 chrome_options = Options()
 chrome_options.add_experimental_option("detach", True)
 driver = webdriver.Chrome(options=chrome_options)
-driver.get("http://www.python.org")
-# assert "Python" in driver.title
-# elem = driver.find_element(By.NAME, "q")
-# elem.clear()
-# elem.send_keys("pycon")
-# elem.send_keys(Keys.RETURN)
-# assert "No results found." not in driver.page_source
-# driver.close()
+driver.get("https://www.google.co.kr/imghp?hl=ko&tab=ri&ogbl")
+driver.maximize_window()
+elem = driver.find_element(By.NAME, "q")
+elem.send_keys("QA")
+elem.send_keys(Keys.RETURN)
+
+images = driver.find_elements(By.CSS_SELECTOR, ".wXeWr.islib.nfEiy")
+count = 1
+for image in images:
+    image.click()
+    time.sleep(3)
+    imgUrl = driver.find_element(By.CSS_SELECTOR,
+                                 ".r48jcc.pT0Scc.iPVvYb").get_attribute("src")
+    image_path = os.path.join(
+        img_folder, "crawling" + str(count) + ".jpg")
+    urllib.request.urlretrieve(imgUrl, image_path)
+    count = count + 1
